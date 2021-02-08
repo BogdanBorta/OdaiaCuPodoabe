63935889e90fd93f2f0257860f609d528477a736
biancacraus <cr_bianca05@yahoo.com>
2021-02-08 23:05:33 +0200

    added changes

--- OdaiaCuPodoabe/OdaiaCuPodoabe/settings.py
+++ OdaiaCuPodoabe/OdaiaCuPodoabe/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
-    'users.apps.UsersConfig',
+    'users',
 ]
 
 MIDDLEWARE = [
--- OdaiaCuPodoabe/users/views.py
+++ OdaiaCuPodoabe/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
+
 def login(request):
     form = UserCreationForm()
     return render(request, 'users/login.html', {'form': form})
+
+def logout(request):
+    return render(request, 'users/logout.html')
