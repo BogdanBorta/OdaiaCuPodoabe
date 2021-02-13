a1e7dc614cd8c8f34de64ad985cde2bd2e483625
biancacraus <cr_bianca05@yahoo.com>
2021-02-13 10:56:17 +0200

    added changes

--- OdaiaCuPodoabe/OdaiaCuPodoabe/settings.py
+++ OdaiaCuPodoabe/OdaiaCuPodoabe/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
-    'users',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'OdaiaCuPodoabe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL='/products'
--- OdaiaCuPodoabe/products/views.py
+++ OdaiaCuPodoabe/products/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1> Tot ok </h1> ")
+
+def products_list(request):
+    return render(request, "products/products_list.html")
