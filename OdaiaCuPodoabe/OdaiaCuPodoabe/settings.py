1f01618c4737cc97c2ee9800ae204a6a1ea44595
BogdanBorta <borta.bogdan11@gmail.com>
2021-02-13 10:58:49 +0200

    register-models-views

--- OdaiaCuPodoabe/OdaiaCuPodoabe/settings.py
+++ OdaiaCuPodoabe/OdaiaCuPodoabe/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'phonenumber_field',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'OdaiaCuPodoabe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PHONENUMBER_DEFAULT_REGION = 'RO'
--- OdaiaCuPodoabe/users/views.py
+++ OdaiaCuPodoabe/users/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+# message.debug, info, succes, warning, error
+from django.contrib import messages
 
 
 def login(request):
-    form = UserCreationForm()
-    return render(request, 'users/login.html', {'form': form})
+
+    return render(request, 'users/login.html')
+
 
 def logout(request):
     return render(request, 'users/logout.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # stocam intr-o variabila username-ul completat de utilizator
+            username = form.cleaned_data.get('username')
+            # afisam un mesaj de creare a contului
+            messages.success(request, f'Account created for {username}!')
+            # redirectionam userul catre pagina cu produse
+            return redirect('products_home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+def home(request):
+    return render(request, 'users/home.html')
