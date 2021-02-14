bced86f26f56d8a38decb596c2ac146cdd7adfc4
biancacraus <cr_bianca05@yahoo.com>
2021-02-14 10:11:41 +0200

    added changes

--- OdaiaCuPodoabe/products/views.py
+++ OdaiaCuPodoabe/products/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# def home(request):
-#     return HttpResponse("<h1> Tot ok </h1> ")
 
 def home(request):
     return render(request, "products/home.html")
-
-
--- OdaiaCuPodoabe/users/views.py
+++ OdaiaCuPodoabe/users/views.py
@@ -8,20 +8,5 @@ def login(request):
 
 def logout(request):
     logout(request)
+    return redirect("products/home.html")
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-
-    context = {'form' : form}
-    return render(request,'registration/register.html', context)
