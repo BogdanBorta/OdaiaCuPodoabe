05a155a475f53b952558114af65c031101820a5c
BogdanBorta <borta.bogdan11@gmail.com>
2021-02-15 15:40:42 +0200

    create_view update 1

--- OdaiaCuPodoabe/products/urls.py
+++ OdaiaCuPodoabe/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home, ProductCreateView, ProductListView
 
+app_name = 'products'
+
 urlpatterns = [
     path("home", home, name='home'),
     path('create_product', ProductCreateView.as_view(), name='product_create'),
