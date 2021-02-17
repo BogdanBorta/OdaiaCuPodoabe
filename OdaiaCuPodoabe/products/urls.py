999ce9a836bbefd518ff7830895e097705948c69
BogdanBorta <borta.bogdan11@gmail.com>
2021-02-17 17:03:58 +0200

    added templates and DetailView

--- OdaiaCuPodoabe/products/urls.py
+++ OdaiaCuPodoabe/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, ProductCreateView, ProductListView
+from .views import home, ProductCreateView, ProductListView, ProductDetailView
 
 app_name = 'products'
 
 urlpatterns = [
     path("home", home, name='home'),
     path('create_product', ProductCreateView.as_view(), name='product_create'),
-    path('products_list',ProductListView.as_view(), name='products_list')
+    path('products_list', ProductListView.as_view(), name='products_list'),
+    path('products_list/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- OdaiaCuPodoabe/products/views.py
+++ OdaiaCuPodoabe/products/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Product
 from django.urls import reverse_lazy
-from django.views.generic import CreateView,ListView
+from django.views.generic import CreateView, ListView, DetailView
+
 
 def home(request):
     product_list = Product.objects.all()
-    return render(request, 'products/home2.html', context={'product_list':product_list})
+    return render(request, 'products/home2.html', context={'product_list': product_list})
+
 
 class ProductCreateView(CreateView):
     template_name = 'products/product_create.html'
@@ -13,7 +15,17 @@ class ProductCreateView(CreateView):
     fields = "__all__"
     success_url = reverse_lazy('products:product_list')
 
+
 class ProductListView(ListView):
     template_name = 'products/products_list.html'
     model = Product
     context_object_name = 'product_list'
+
+
+class ProductDetailView(DetailView):
+    template_name = 'products/product_detail.html'
+    model = Product
+    context_object_name = 'product_detail'
+
+
+
