5ad335e4f5076a9e9ceb435c8f315a698ba80f0a
biancacraus <cr_bianca05@yahoo.com>
2021-02-20 08:54:46 +0200

    added details

--- OdaiaCuPodoabe/products/urls.py
+++ OdaiaCuPodoabe/products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import ProductDetailView, ProductDeleteView, ProductUpdateView, home
 
 urlpatterns = [
-    path("home", views.home, name='home')
+    path("home", home, name='home'),
+    path("details/<int:pk>", ProductDetailView.as_view(), name="product_details"),
+    path("delete/<int:pk>", ProductDeleteView.as_view(), name="product_delete"),
+    path("update/<int:pk>", ProductUpdateView.as_view(), name="product_update"),
 ]
--- OdaiaCuPodoabe/products/views.py
+++ OdaiaCuPodoabe/products/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from .models import Product
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 def home(request):
     product_list = Product.objects.all()
     return render(request, 'products/home2.html', context={'product_list':product_list})
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_details.html"
+    context_object_name = "product"
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "products/product_delete.html"
+    context_object_name = "product"
+    success_url = reverse_lazy("products: product_list")
+    fields = ["name","price","description","image"]
+    pk_url_kwarg = "product_id"
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = "products/product_update.html"
+    context_object_name = "product"
+    success_url = reverse_lazy("products: product_list")
+    fields = ["name", "price", "description", "image"]
