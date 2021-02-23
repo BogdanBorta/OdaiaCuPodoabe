db641be9004006a04ca08ed3638c56ff34c195f0
BogdanBorta <borta.bogdan11@gmail.com>
2021-02-23 09:41:43 +0200

    small functionality changes

--- OdaiaCuPodoabe/users/views.py
+++ OdaiaCuPodoabe/users/views.py
@@ -1,26 +1,28 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
 
 def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(username=username,password=password)
+        user = authenticate(username=username, password=password)
         if user != None:
             login(request, user)
             return redirect("products:product_list")
-    return render(request, 'users/login.html')
+    return render(request, 'users/login.html', {})
 
 
 def logout_view(request):
     logout(request)
-    return redirect("users/welcome.html")
+    return redirect("welcome_page")
 
 
 def welcome_page(request):
-    return render(request, 'users/welcome.html',)
+    return render(request, 'users/welcome.html', )
+
 
 def register_view(request):
     if request.method == 'POST':
@@ -32,9 +34,7 @@ def register_view(request):
             # afisam un mesaj de creare a contului
             messages.success(request, f'Contul {username} a fost creat!')
             # redirectionam userul catre pagina cu produse
-            return redirect('home')
+            return redirect('users:login')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
-
-
