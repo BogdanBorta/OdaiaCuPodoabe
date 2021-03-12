3e7841b4df3b7280556b9706cd00a2ec5d041ee1
BogdanBorta <borta.bogdan11@gmail.com>
2021-03-12 21:45:27 +0200

    added stiling and social auth

--- users/forms.py
+++ users/forms.py
@@ -3,22 +3,21 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserForm(UserCreationForm):
+class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username',
-                  'email', 'password1', 'password2')
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2'
+        )
 
-        # Am adaugat clasa campurilor, pentru a le putea selecta in css(nu functioneaza)
-        widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'user_form'}),
-            'last_name': forms.TextInput(attrs={'class': 'user_form'}),
-            'username': forms.TextInput(attrs={'class': 'user_form'}),
-            'email': forms.TextInput(attrs={'class': 'user_form'}),
-            'password1': forms.TextInput(attrs={'class': 'user_form'}),
-            'password2': forms.TextInput(attrs={'class': 'user_form'})
-        }
+
+
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import login_view,logout_view,register_view
+from .views import login_view, logout_view, register_view, profile_view, edit_profile_view
+
 
 urlpatterns = [
     path("login", login_view, name="login_view"),
     path("logout", logout_view, name="logout_view"),
     path("register/", register_view, name='register'),
+    path("profile/", profile_view, name='profile_view'),
+    path("edit_profile/", edit_profile_view, name='edit_profile_view')
 ]
