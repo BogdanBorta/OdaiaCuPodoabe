0df5e9b6c73b09dd57365e4c3fd927331c020e1b
BogdanBorta <borta.bogdan11@gmail.com>
2021-04-06 21:25:52 +0300

    debug false

--- OdaiaCuPodoabe/settings.py
+++ OdaiaCuPodoabe/settings.py
@@ -20,9 +20,6 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY') # hiding secret keys in env file
 
